3ccde9f6e65a6701e229e4d2168e2505912f1278
ksh <hyun031916@gmail.com>
2020-09-05 00:30:12 +0900

    #2 enabled, clickable 속성의 XML 코드

--- app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
+++ app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
@@ -11,20 +11,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
-    EditText et;
+//    TextView tv;
+//    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView)findViewById(R.id.textView);
-        et = (EditText)findViewById(R.id.editText);
+//        tv = (TextView)findViewById(R.id.textView);
+//        et = (EditText)findViewById(R.id.editText);
         Button button1 = (Button)findViewById(R.id.button1);
         Button button2 = (Button)findViewById(R.id.button2);
         Button button3 = (Button)findViewById(R.id.button3);
-        Button button4 = (Button)findViewById(R.id.button4);
-        Button button5 = (Button)findViewById(R.id.button5);
+//        Button button4 = (Button)findViewById(R.id.button4);
+//        Button button5 = (Button)findViewById(R.id.button5);
 
     }
 
