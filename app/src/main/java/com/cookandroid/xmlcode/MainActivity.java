c49729e2ad1244b2d2b06e67dc44fd4ee8bffb20
ksh <hyun031916@gmail.com>
2020-09-05 00:36:38 +0900

    #3 rotation 속성의 XML 코드

--- app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
+++ app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button button1 = (Button)findViewById(R.id.button1);
         Button button2 = (Button)findViewById(R.id.button2);
         Button button3 = (Button)findViewById(R.id.button3);
-//        Button button4 = (Button)findViewById(R.id.button4);
+        Button button4 = (Button)findViewById(R.id.button4);
 //        Button button5 = (Button)findViewById(R.id.button5);
 
     }
