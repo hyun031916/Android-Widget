88666fcd1e4ebbb1a0daa8ab697c7ca816521a80
ksh <hyun031916@gmail.com>
2020-09-04 23:52:14 +0900

    #layout_margin 속성의 XML 코드 기본 위젯 익히기(TextView, EditText, Button 사용)

--- app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
+++ app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
@@ -3,12 +3,33 @@ package com.cookandroid.xmlcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
+    EditText et;
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.textView);
+        et = (EditText)findViewById(R.id.editText);
+        bt = (Button)findViewById(R.id.button1);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "전송되었습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
