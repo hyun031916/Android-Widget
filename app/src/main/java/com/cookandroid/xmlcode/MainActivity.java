e8ee5d8b752b36ea4fd5ff0562358e141026c0e0
ksh <hyun031916@gmail.com>
2020-09-05 02:36:18 +0900

    #6 Switch와 ToggleButton의 XML 코드

--- app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
+++ app/src/main/java/com/cookandroid/xmlcode/MainActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,19 +30,22 @@ public class MainActivity extends AppCompatActivity {
 //        Button button4 = (Button)findViewById(R.id.button4);
 //        Button button5 = (Button)findViewById(R.id.button5);
 
-        TextView textView1 = (TextView)findViewById(R.id.textView1);
-        TextView textView2 = (TextView)findViewById(R.id.textView2);
-        TextView textView3 = (TextView)findViewById(R.id.textView3);
-        TextView textView4 = (TextView)findViewById(R.id.textView4);
-        TextView textView5 = (TextView)findViewById(R.id.textView5);
+//        TextView textView1 = (TextView)findViewById(R.id.textView1);
+//        TextView textView2 = (TextView)findViewById(R.id.textView2);
+//        TextView textView3 = (TextView)findViewById(R.id.textView3);
+//        TextView textView4 = (TextView)findViewById(R.id.textView4);
+//        TextView textView5 = (TextView)findViewById(R.id.textView5);
+//
+//        textView1.setText("안녕하세요?");
+//        textView1.setTextColor(Color.RED);
+//        textView2.setTextSize(30);
+//        textView2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
+//        textView3.setText("가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하");
 
-        textView1.setText("안녕하세요?");
-        textView1.setTextColor(Color.RED);
-        textView2.setTextSize(30);
-        textView2.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD_ITALIC);
-        textView3.setText("가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하");
-        textView3.setSingleLine();
-
+        Switch switch1 = (Switch)findViewById(R.id.switch1);
+        Switch switch2 = (Switch)findViewById(R.id.switch2);
+        ToggleButton toggleBtn1 = (ToggleButton)findViewById(R.id.toggleBtn1);
+        ToggleButton toggleBtn2 = (ToggleButton)findViewById(R.id.toggleBtn2);
     }
 
 }
